c5eb5a237b1e2e76b97490e3b75a298ca7d74e17
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-03-31 17:04:22 +0700

    Login App.js

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
-import { Alert, Button, TextInput, View, StyleSheet, ImageBackground, Image } from 'react-native';
+import React,  { useState } from 'react';
+import { Alert, TouchableOpacity, TextInput, View, StyleSheet, ImageBackground, Image, Text } from 'react-native';
 
 
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
+
+const  App = () => {
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   
+
+
+
+
   
-  onLogin() {
+  const onLogin = () => {
      
-    const loginInfo = this.state;
+    const loginInfo = {username, password};
     if (loginInfo.username === 'admin' && loginInfo.password === '123'){
       Alert.alert('Welcome!', `${loginInfo.username} + ${loginInfo.password}`);
     } else {Alert.alert('No', `${loginInfo.username} + ${loginInfo.password}`);}
@@ -24,37 +24,42 @@ export default class App extends Component {
     
   }
 
-  render() {
+ 
     return (
       <View style={styles.container} >
         <ImageBackground source={require('./images/login-bg.png')} style={styles.loginBackground}>
           <Image source={require('./images/app_logo.png')}/>
-        <View>
+        <View style={styles.inputView}>
+          <Text style={styles.loginText}>Login</Text>
         <TextInput
-          value={this.state.username}
-          onChangeText={(username) => this.setState({ username })}
+          value = {username}
+          onChangeText={(username) => setUsername(username)}
           placeholder={'Username'}
           style={styles.input}
         />
         <TextInput
-          value={this.state.password}
-          onChangeText={(password) => this.setState({ password })}
+          value={password}
+          onChangeText={(password) => setPassword(password)}
           placeholder={'Password'}
           secureTextEntry={true}
           style={styles.input}
         />
         </View>  
-        <Button
+        <TouchableOpacity
           title={'Login'}
           style={styles.loginButton}
-          onPress={this.onLogin.bind(this)}
-        />
+         onPress={onLogin}
+        ><Text style={styles.loginText}>GO!</Text></TouchableOpacity>
 
     </ImageBackground>
       </View>
     );
-  }
-}
+
+
+
+
+
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -68,17 +73,33 @@ const styles = StyleSheet.create({
     
     
   },
+  inputView : {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    width: '80%',
+    height: '40%',
+    borderRadius: 30,
+    borderColor: 'white',
+    borderWidth: 1,
+
+  }
+  ,
   input: {
+    
     fontSize: 16,
     padding: 10,
     textAlign: 'center',
-    width: 350,
+    width: '70%',
     height: 60,
-    backgroundColor: 'rgba(255, 255, 255, 0.4)',
-    borderRadius: 30,
+    // backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    borderColor: 'rgba(255, 255, 255, 0)',
+    borderBottomColor: 'white',
+  
+    borderWidth: 2,
     marginBottom: 10,
-    borderColor: 'white',
-    borderWidth: 1,
+   
     
   },
   loginBackground: {
@@ -94,12 +115,27 @@ const styles = StyleSheet.create({
   },
 
   loginButton: {
-    backgroundColor: '#e1e1e1',
-    width: 100,
-    height: 60
+    color: 'blue',
+    width: "60%",
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: 'white',
+    height: 60,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor:'rgba(255, 255, 255, 0.4)',
+  },
 
+  loginText: {
+     
+    fontSize: 18,
+    color: '#614AD3',
+    fontWeight: 'bold'
   }
 
 
  
 });
+
+export default App;

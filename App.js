85ca43a2016044109b180411add8eb258b8a2fcf
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2021-03-31 08:43:20 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Alert, Button, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Button, TextInput, View, StyleSheet, ImageBackground, Image } from 'react-native';
+
+
 
 export default class App extends Component {
   constructor(props) {
@@ -12,14 +14,19 @@ export default class App extends Component {
   }
   
   onLogin() {
+     
     const { username, password } = this.state;
-
-    Alert.alert('Credentials', `${username} + ${password}`);
+   
+    Alert.alert('No', `${username} + ${password}`);
+    
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} >
+        <ImageBackground source={require('./images/login-bg.png')} style={styles.loginBackground}>
+          <Image source={require('./images/app_logo.png')}/>
+        <View>
         <TextInput
           value={this.state.username}
           onChangeText={(username) => this.setState({ username })}
@@ -33,12 +40,14 @@ export default class App extends Component {
           secureTextEntry={true}
           style={styles.input}
         />
-        
+        </View>  
         <Button
           title={'Login'}
-          style={styles.input}
+          style={styles.loginButton}
           onPress={this.onLogin.bind(this)}
         />
+
+    </ImageBackground>
       </View>
     );
   }
@@ -46,17 +55,48 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+   
     flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#e1e1e1'
+   
+    
+    
   },
   input: {
-    width: 200,
-    height: 44,
+    fontSize: 16,
     padding: 10,
-    borderWidth: 1,
-    borderColor: 'black',
+    textAlign: 'center',
+    width: 350,
+    height: 60,
+    backgroundColor: 'rgba(255, 255, 255, 0.4)',
+    borderRadius: 30,
     marginBottom: 10,
+    borderColor: 'white',
+    borderWidth: 1,
+    
   },
+  loginBackground: {
+    // flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    width: '100%',
+    height: '100%',
+    resizeMode: 'center',
+    // justifyContent: 'center'
+  },
+
+  loginButton: {
+    backgroundColor: '#e1e1e1',
+    width: 100,
+    height: 60
+
+  }
+
+
+ 
 });
